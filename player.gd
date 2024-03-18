f3ed538f3a866bac34b0ba19eb15e3a6b6a7e20b
l4islim4 <laislima12321@gmail.com>
2024-03-18 11:57:34 -0300

    wip

---
+++ player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+const SPEED = 100.0
+const ACCELERATION = 600.0
+const FRICTION = 1000.0
+const JUMP_VELOCITY = -300.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+	else:
+		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
+			velocity.y = JUMP_VELOCITY / 2
+
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction != 0:
+		velocity.x = move_toward(velocity.x, SPEED * direction, ACCELERATION * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+
+	move_and_slide()

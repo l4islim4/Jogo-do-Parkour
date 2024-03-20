9273c5c0aa024fed17a22af443f2904c519a6b19
l4islim4 <laislima12321@gmail.com>
2024-03-20 11:58:31 -0300

    atualização n.1

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
-const ACCELERATION = 600.0
+const ACCELERATION = 800.0
 const FRICTION = 1000.0
 const JUMP_VELOCITY = -300.0
 
@@ -9,20 +9,29 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	apply_gravity(delta)
+	handle_jump()
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	handle_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
+	move_and_slide()
+	
+func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+func handle_jump():
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
 	else:
 		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
-
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction != 0:
-		velocity.x = move_toward(velocity.x, SPEED * direction, ACCELERATION * delta)
-	else:
+			
+func apply_friction(input_axis, delta):
+	if input_axis == 0:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
-
-	move_and_slide()
+		
+func handle_acceleration(input_axis, delta):
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
